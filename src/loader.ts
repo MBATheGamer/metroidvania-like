29bd94d665e25eaf36584dd610eeba379f8a2758
MBATheGamer <mbathegamer@gmail.com>
2024-07-02 06:13:21 +0100

    Load player

--- src/loader.ts
+++ src/loader.ts
@@ -11,3 +11,16 @@ export const k = kaboom({
 });
 
 k.loadFont("glyphmesss", "./assets/glyphmesss.tff");
+
+k.loadSprite("player", "./assets/sprites/u.png", {
+  sliceX: 8,
+  sliceY: 9,
+  anims: {
+    idle: { from: 0, to: 7, loop: true },
+    run: { from: 8, to: 13, loop: true },
+    jump: { from: 51, to: 51, loop: true },
+    fall: { from: 54, to: 54, loop: true },
+    explode: { from: 64, to: 69 },
+    attack: { from: 24, to: 28, speed: 16 },
+  },
+});
