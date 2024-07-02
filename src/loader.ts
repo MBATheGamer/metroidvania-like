91f10ea198427773571047762606c65d357f658c
MBATheGamer <mbathegamer@gmail.com>
2024-07-02 06:06:38 +0100

    Fix: kaboom pixel padding

--- src/loader.ts
+++ src/loader.ts
@@ -3,8 +3,9 @@ import kaboom from "kaboom";
 export const SCALE = 2;
 
 export const k = kaboom({
-  width: 640,
-  height: 360,
+  width: 640 * SCALE,
+  height: 360 * SCALE,
+  scale: SCALE,
   letterbox: true,
   global: false,
 });
