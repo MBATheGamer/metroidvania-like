ea470b52c98fb66ae806b60d64661a7efc19f3b1
MBATheGamer <mbathegamer@gmail.com>
2024-07-04 20:42:53 +0100

    Add polygon collider logic

--- src/scenes/room-utils.ts
+++ src/scenes/room-utils.ts
@@ -1,4 +1,4 @@
-import { GameObj, KaboomCtx } from "kaboom";
+import { GameObj, KaboomCtx, Vec2 } from "kaboom";
 import { TiledObject } from "tiled-types";
 
 export const setBackgroundColor = (
@@ -18,6 +18,25 @@ export const setMapColliders = (
   colliders: TiledObject[]
 ) => {
   for (const collider of colliders) {
+    if (collider.polygon) {
+      const coordinates: Vec2[] = [];
+
+      for (const point of collider.polygon) {
+        coordinates.push(kaboom.vec2(point.x, point.y));
+      }
+
+      map.add([
+        kaboom.pos(collider.x, collider.y),
+        kaboom.area({
+          shape: new kaboom.Polygon(coordinates),
+          collisionIgnore: ["collider"],
+        }),
+        "collider",
+        collider.type,
+      ]);
+      continue;
+    }
+
     map.add([
       kaboom.pos(collider.x, collider.y),
       kaboom.area({
