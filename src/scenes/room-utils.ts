6d87e52f421b08e39e6e36634a8393f62a6d0a36
MBATheGamer <mbathegamer@gmail.com>
2024-07-04 20:42:02 +0100

    Add collider logic

--- src/scenes/room-utils.ts
+++ src/scenes/room-utils.ts
@@ -16,4 +16,17 @@ export const setMapColliders = (
   kaboom: KaboomCtx,
   map: GameObj,
   colliders: TiledObject[]
-) => {};
+) => {
+  for (const collider of colliders) {
+    map.add([
+      kaboom.pos(collider.x, collider.y),
+      kaboom.area({
+        shape: new kaboom.Rect(kaboom.vec2(0), collider.width, collider.height),
+        collisionIgnore: ["collider"],
+      }),
+      kaboom.body({ isStatic: true }),
+      "collider",
+      collider.type,
+    ]);
+  }
+};
