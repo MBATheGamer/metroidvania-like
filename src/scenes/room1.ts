842f47cdf0de31bcb853e0dd9b259019477c32d2
MBATheGamer <mbathegamer@gmail.com>
2024-07-04 20:53:06 +0100

    Add camera to room1 scene

--- src/scenes/room1.ts
+++ src/scenes/room1.ts
@@ -5,6 +5,10 @@ import { setBackgroundColor, setMapColliders } from "./room-utils";
 export const room1 = (kaboom: KaboomCtx, data: TiledMap) => {
   setBackgroundColor(kaboom, 0xa2aed5);
 
+  kaboom.camScale(4, 4);
+  kaboom.camPos(170, 100);
+  kaboom.setGravity(1000);
+
   const layers = data.layers;
 
   const map = kaboom.add([kaboom.pos(0, 0), kaboom.sprite("room1")]);
